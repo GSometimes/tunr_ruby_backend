179db926dc7ce2e3553aa296954c61f6eb6daffd
Gourav <gore.auluck@gmail.com>
2020-11-17 08:48:35 -0800

    cheese

---
+++ db/seeds.rb
@@ -0,0 +1,15 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Song.create([
+    { name: 'Blank Space', artist: "Taylor Swift", length:"3:51", favorite:false},
+    { name: 'Style', artist: "Taylor Swift", length:"3:51", favorite:false},
+    { name: 'Shake It Off', artist: "Taylor Swift", length:"3:39", favorite:false},
+    { name: 'Bad Blood', artist: "Taylor Swift", length:"3:31", favorite:false},
+    { name: 'I Wish You Would', artist: "Taylor Swift", length:"3:27", favorite:false},
+  ])
